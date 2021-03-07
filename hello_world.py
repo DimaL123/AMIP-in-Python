5d424fb8573518ad6d55b09b68463bced0691414
DimaL123 <litvinenkodimaimmm@gmail.com>
2021-03-07 11:47:08 +0200

    add komentarii

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,8 @@
-print('You name?')
-q= input()
+print('You name?')                  ##  'vyvodit You name?'
+q= input()                          ## 'schityvaet s klaviatury'
 
-print('how old are you')
-w= input()
-
-
-print('Hello world from '+ q +'!')
+print('how old are you')            ##' vivodit how old are you'
+w= input()                          ## 'schityvaet s klaviatury'
 
 
-
+print('Hello world from '+ q +'!')  ##'vyvodit Hello world from i schitanoe imja'
